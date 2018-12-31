23d022d6f6acd89293640dcbe0b76ea51b45afa5
cjdlt <1537981545@qq.com>
2018-12-31 17:26:28 +0800

    chijiandelongtao

--- src/main/java/com/giteclipse/TestEclipse.java
+++ src/main/java/com/giteclipse/TestEclipse.java
@@ -18,7 +18,10 @@ package com.giteclipse;
 public class TestEclipse {
 
 	public static void main(String[] args) {
+		
                 System.out.println(1);
+                
+                System.out.println(22323232);
 	}
 
 	
