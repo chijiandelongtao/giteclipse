2bd56515fdc2256c9404637c0762a58d20d406e3
cjdlt <1537981545@qq.com>
2019-01-01 21:31:34 +0800

    haha

--- src/main/java/com/giteclipse/MI5299.java
+++ src/main/java/com/giteclipse/MI5299.java
@@ -5,5 +5,10 @@ public class MI5299 {
 	public static void main(String[] args) {
 		System.out.println("测试另一台电脑提交");
 		System.out.println("11");
+	
+	}
+	
+	public static void test() {
+		System.out.println("方阿飞");
 	}
 }
